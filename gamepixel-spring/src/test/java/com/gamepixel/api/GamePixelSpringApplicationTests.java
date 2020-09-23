9c0cb9c4a48082c65e5f834d6795d03b95074fee
David Oppong <doppong791@outlook.com>
2020-09-23 16:14:10 -0400

    setup env

---
+++ gamepixel-spring/src/test/java/com/gamepixel/api/GamePixelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamepixel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePixelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
