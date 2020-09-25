f31e0a8d41fbb1d47bffbc940eef9b771304d5e3
David Oppong <doppong791@outlook.com>
2020-09-25 18:30:45 -0400

    cont, repo, srv, and tests

--- gamepixel-spring/src/main/java/com/gamepixel/api/controller/GamerController.java
+++ gamepixel-spring/src/main/java/com/gamepixel/api/controller/GamerController.java
@@ -1,17 +1,62 @@
 package com.gamepixel.api.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import com.gamepixel.api.models.Gamer;
 import com.gamepixel.api.service.GamerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Controller
+/**
+ * POST - add gamer to db GET - retrieving all gamers GET - retrieving specified
+ * gamer DELETE - delete consumer from db
+ * 
+ */
+
+@RestController
+@RequestMapping(value = "/gamers")
 public class GamerController {
-
+    /**
+     * Optimization add Lombok annotation (@AllArgsConstructor) Remove @Autowired
+     * since Lombok Remove Constructor remove all requestmapping Use Response Entity
+     * for controller
+     */
     @Autowired
     GamerService gamerService;
 
     public GamerController(GamerService gamerService) {
         this.gamerService = gamerService;
     }
+
+    @GetMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<List<Gamer>> getAllGamers() {
+        return ResponseEntity.status(HttpStatus.OK).body(gamerService.retrieveAllGamers());
+    }
+
+    @RequestMapping(value = "/gamers/{user_id}", method = RequestMethod.GET)
+    public Gamer getGamerById(@RequestParam(value = "user_id") Integer user_id) {
+        return gamerService.retrieveById(user_id);
+    }
+
+    @RequestMapping(value = "/gamers/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Gamer addGamer(@RequestBody Gamer newGamer) {
+        return gamerService.createGamer(newGamer);
+    }
+
+    @RequestMapping(value = "/gamers/{user_id}", method = RequestMethod.DELETE)
+    public void deleteGamerFromDb(@RequestParam(value = "user_id") Integer user_id) {
+        gamerService.deleteGamer(user_id);
+    }
+
 }
--- gamepixel-spring/src/main/java/com/gamepixel/api/repository/GamerRepo.java
+++ gamepixel-spring/src/main/java/com/gamepixel/api/repository/GamerRepo.java
@@ -3,7 +3,9 @@ package com.gamepixel.api.repository;
 import com.gamepixel.api.models.Gamer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GamerRepo extends JpaRepository<Gamer, Integer> {
 
 }
